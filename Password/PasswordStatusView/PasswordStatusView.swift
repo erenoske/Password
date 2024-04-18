53b8b642be7fa688fbeb264ebb0aa0f73c6c5d18
Eren İşgören <erenisgoren@gmail.com>
2024-04-18 16:44:54 +0300

    Reset password page

---
+++ Password/PasswordStatusView/PasswordStatusView.swift
@@ -0,0 +1,71 @@
+//
+//  PasswordStatusView.swift
+//  Password
+//
+//  Created by eren on 18.04.2024.
+//
+
+import Foundation
+import UIKit
+
+class PasswordStatusView: UIView {
+    
+    let stackView = UIStackView()
+    let lengthCriteriaView = PasswordCriteriaView(text: "8-32 characters (no spaces)")
+    let uppercaseCriteriaView = PasswordCriteriaView(text: "uppercase letter (A-Z)")
+    let lowerCaseCriteriaView = PasswordCriteriaView(text: "lowercase (a-z)")
+    let digitCriteriaView = PasswordCriteriaView(text: "digit (0-9)")
+    let specialCharacterCriteriaView = PasswordCriteriaView(text: "special character (e.g. !@#$%^)")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension PasswordStatusView {
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .tertiarySystemFill
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.distribution = .equalCentering
+        
+        lengthCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        uppercaseCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        lowerCaseCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        digitCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        specialCharacterCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(lengthCriteriaView)
+        stackView.addArrangedSubview(uppercaseCriteriaView)
+        stackView.addArrangedSubview(lowerCaseCriteriaView)
+        stackView.addArrangedSubview(digitCriteriaView)
+        stackView.addArrangedSubview(specialCharacterCriteriaView)
+        addSubview(stackView)
+        
+        // Stack layout
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 2)
+        ])
+        
+    }
+}
